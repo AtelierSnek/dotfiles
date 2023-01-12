28f592e8aafce09c3b4527ec61fae13bd08c7761
Rin <rin@rin.systems>
2023-01-12 18:11:25 +1100

    fix error in logic

--- wal/bgStuff.sh
+++ wal/bgStuff.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-wal -i /mnt/data/walls/1440 --backend colorthief --cols16
+if [ -d /mnt/data/walls/1440 ]; then
+  export WALLS_DIR="/mnt/data/walls/1440"
+else
+  export WALLS_DIR="$HOME/walls"
+fi
+echo $WALLS_DIR
+wal -i $WALLS_DIR --backend colorthief --cols16
+
 cp ~/.cache/wal/colors-waybar.css ~/.config/waybar/colors-waybar.css
