040025abbf01361f5886364f6b1d7f5a454c2bca
Rin <rin@rin.systems>
2023-01-12 16:46:27 +1100

    update rofi config for wal

--- wal/bgStuff.sh
+++ wal/bgStuff.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wal -i /mnt/data/walls/1440 --backend colorz
+wal -i /mnt/data/walls/1440 --backend colorthief --cols16
 cp ~/.cache/wal/colors-waybar.css ~/.config/waybar/colors-waybar.css
