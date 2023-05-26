809b3a49dd6e2518150f0cdbdc597863cd53c0d5
Rin <rin@rin.systems>
2023-05-26 14:57:05 +1000

    eventually consistent

--- wal/bgStuff.sh
+++ wal/bgStuff.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-if [ -d /mnt/data/walls/1440 ]; then
-  export WALLS_DIR="/mnt/data/walls/1440"
-else
-  export WALLS_DIR="$HOME/walls"
-fi
+export WALLS_DIR="$HOME/walls"
 wal -i $WALLS_DIR --backend colorz --cols16
 
 cp ~/.cache/wal/colors-waybar.css ~/.config/waybar/colors-waybar.css
