e7d30d1cdffc35d18ca6cd7afaaded89cfcf64cd
Rin <rin@rin.systems>
2023-06-13 10:57:57 +1000

    add saturation control on bg script

--- wal/bgStuff.sh
+++ wal/bgStuff.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export WALLS_DIR="$HOME/walls"
-wal -i $WALLS_DIR --backend colorz --cols16
+wal -i $WALLS_DIR --backend colorz --cols16 --saturate 0.3
 
 cp ~/.cache/wal/colors-waybar.css ~/.config/waybar/colors-waybar.css
