c6b387f4e4a121f00426ddc423575a458106c8a2
Rin <rin@rin.systems>
2024-05-14 22:44:53 +1000

    fix background generation for new pywal and python

--- wal/bgStuff.sh
+++ wal/bgStuff.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export WALLS_DIR="$HOME/walls"
-wal -i $WALLS_DIR --backend colorz --cols16 --saturate 0.3
+wal -i $WALLS_DIR --cols16 --saturate 0.3
 
 # App Specific Links
 cp ~/.cache/wal/colors-waybar.css ~/.config/waybar/colors-waybar.css
