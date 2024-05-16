2f406557702150a06ab3fd42db229b0938c0cc6a
Rin <rin@rin.systems>
2024-05-16 14:17:41 +1000

    increase saturation for bgs

--- wal/bgStuff.sh
+++ wal/bgStuff.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export WALLS_DIR="$HOME/walls"
-wal -i $WALLS_DIR --cols16 --saturate 0.3
+wal -i $WALLS_DIR --cols16 --saturate 0.4
 
 # App Specific Links
 cp ~/.cache/wal/colors-waybar.css ~/.config/waybar/colors-waybar.css
