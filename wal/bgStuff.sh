2b0ed494bfc0ee171b91ab136ac0486359f2fc1b
Rin <rin@rin.systems>
2024-09-15 14:16:43 +1000

    set wal to quiet mode for faster desktop startup

--- wal/bgStuff.sh
+++ wal/bgStuff.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export WALLS_DIR="$HOME/walls"
-wal -i $WALLS_DIR --backend fast-colorthief --cols16
+wal -i $WALLS_DIR --backend wal --cols16 lighten --contrast 1.25 -q
 
 # App Specific Links
 cp ~/.cache/wal/colors-waybar.css ~/.config/waybar/colors-waybar.css
