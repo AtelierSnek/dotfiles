70bc984e42a21e79701162c1153972b300ced2c4
Rin <rin@rin.systems>
2025-02-27 13:06:08 +1100

    wal: update algo

--- wal/bgStuff.sh
+++ wal/bgStuff.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export WALLS_DIR="$HOME/walls"
-wal -i $WALLS_DIR --backend wal --cols16 lighten --contrast 1.25 -q
+wal -i $WALLS_DIR --backend haishoku --cols16 lighten
+
 
 # App Specific Links
 cp ~/.cache/wal/colors-waybar.css ~/.config/waybar/colors-waybar.css
