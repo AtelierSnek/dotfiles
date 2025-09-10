a96fa98b869b20a0af348bc953a4b8f6ca3943ad
Rin <rin@rin.systems>
2025-09-10 17:45:26 +1000

    rofi: add rofi config

--- wal/bgStuff.sh
+++ wal/bgStuff.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export WALLS_DIR="$HOME/walls"
-wal -i $WALLS_DIR --backend haishoku --cols16 lighten
+  wal -i $WALLS_DIR --backend haishoku --cols16 darken --contrast 1.0
 
 
-# App Specific Links
-cp ~/.cache/wal/colors-waybar.css ~/.config/waybar/colors-waybar.css
+  # App Specific Links
+  cp ~/.cache/wal/colors-waybar.css ~/.config/waybar/colors-waybar.css
