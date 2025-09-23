1bff4e6ad3d9352dcb7c724cdb3f1a16176a941d
Rin <rin@rin.systems>
2025-09-23 17:46:45 +1000

    wal: rofi theme + change backend

--- wal/bgStuff.sh
+++ wal/bgStuff.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export WALLS_DIR="$HOME/walls"
-  wal -i $WALLS_DIR --backend haishoku --cols16 darken --contrast 1.0
+  wal -i $WALLS_DIR --backend fast-colorthief --cols16 darken -a 90
 
 
   # App Specific Links
