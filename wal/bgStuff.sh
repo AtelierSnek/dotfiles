8a924dce0945fdb41afd99916779c6ceda681fbe
Rin <rin@rin.systems>
2026-01-18 00:41:24 +1100

    wal: increase saturation slightly

--- wal/bgStuff.sh
+++ wal/bgStuff.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export WALLS_DIR="$HOME/walls"
-  wal -i "$WALLS_DIR" -q --backend haishoku --cols16 dual -a 90 --saturate -0.05
+  wal -i "$WALLS_DIR" --backend haishoku --cols16 dual -a 90 --saturate 0.05
 
 
   # App Specific Links
